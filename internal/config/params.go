462e07dcda60268586a0304acd2f0350d44a2d81
astimergalieva <astimergalieva@domclick.ru>
2024-08-09 23:01:40 +0300

    add json config

--- internal/config/params.go
+++ internal/config/params.go
@@ -2,18 +2,56 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"io"
+	"log"
+	"os"
 
 	"github.com/caarlos0/env/v6"
 )
 
 // ServerConfig - тип для сохранения настроек сервиса
 type ServerConfig struct {
-	BaseAddress      string `env:"SERVER_ADDRESS"`
-	ShortURLsAddress string `env:"BASE_URL"`
-	FileStoragePath  string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN      string `env:"DATABASE_DSN"`
-	EnableHTTPS      bool   `env:"ENABLE_HTTPS"`
+	BaseAddress      string `env:"SERVER_ADDRESS" json:"server_address"`
+	ShortURLsAddress string `env:"BASE_URL" json:"base_url"`
+	FileStoragePath  string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DatabaseDSN      string `env:"DATABASE_DSN" json:"database_dsn"`
+	EnableHTTPS      bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+	ConfigFile       string `env:"CONFIG"`
+}
+
+func parseFileParams(name string) ServerConfig {
+	jsonFile, err := os.Open(name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var config ServerConfig
+
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
+type configValue interface {
+	~bool | ~string
+}
+
+func firstNotZero[T configValue](values ...T) T {
+	var zero T
+	for _, v := range values {
+		if v != zero {
+			return v
+		}
+	}
+	return zero
 }
 
 // NewConfig инициализация объекта ServerConfig. Параметры берутся из env, либо аргументов командной строки
@@ -26,29 +64,27 @@ func NewConfig() (*ServerConfig, error) {
 
 	var commandLineParams ServerConfig
 
-	flag.StringVar(&commandLineParams.BaseAddress, "a", "localhost:8080", "Base address to listen on")
-	flag.StringVar(&commandLineParams.ShortURLsAddress, "b", "http://localhost:8080", "Short URLs base address")
-	flag.StringVar(&commandLineParams.FileStoragePath, "f", "/tmp/short-url-db.json", "Path to file to store urls")
+	flag.StringVar(&commandLineParams.BaseAddress, "a", "", "Base address to listen on")
+	flag.StringVar(&commandLineParams.ShortURLsAddress, "b", "", "Short URLs base address")
+	flag.StringVar(&commandLineParams.FileStoragePath, "f", "", "Path to file to store urls")
 	flag.StringVar(&commandLineParams.DatabaseDSN, "d", "", "Database DSN")
 	flag.BoolVar(&commandLineParams.EnableHTTPS, "s", false, "Enable HTTPS")
+	flag.StringVar(&commandLineParams.ConfigFile, "c", "", "Config file")
 	flag.Parse()
 
-	if params.BaseAddress == "" {
-		params.BaseAddress = commandLineParams.BaseAddress
+	if params.ConfigFile == "" {
+		params.ConfigFile = commandLineParams.ConfigFile
 	}
-	if params.ShortURLsAddress == "" {
-		params.ShortURLsAddress = commandLineParams.ShortURLsAddress
-	}
-	if params.FileStoragePath == "" {
-		params.FileStoragePath = commandLineParams.FileStoragePath
-	}
-	if params.DatabaseDSN == "" {
-		params.DatabaseDSN = commandLineParams.DatabaseDSN
+	var fileParams ServerConfig
+	if params.ConfigFile != "" {
+		fileParams = parseFileParams(params.ConfigFile)
 	}
 
-	if !params.EnableHTTPS {
-		params.EnableHTTPS = commandLineParams.EnableHTTPS
-	}
+	params.BaseAddress = firstNotZero(params.BaseAddress, commandLineParams.BaseAddress, fileParams.BaseAddress)
+	params.ShortURLsAddress = firstNotZero(params.ShortURLsAddress, commandLineParams.ShortURLsAddress, fileParams.ShortURLsAddress)
+	params.FileStoragePath = firstNotZero(params.FileStoragePath, commandLineParams.FileStoragePath, fileParams.FileStoragePath)
+	params.DatabaseDSN = firstNotZero(params.DatabaseDSN, commandLineParams.DatabaseDSN, fileParams.DatabaseDSN)
+	params.EnableHTTPS = firstNotZero(params.EnableHTTPS, commandLineParams.EnableHTTPS, fileParams.EnableHTTPS)
 
 	return &params, nil
 }
